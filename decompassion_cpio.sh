76146578c3b612171191b50160055f53c955c2fc
Bethune <108174641+SuzumiyaAsahi@users.noreply.github.com>
2025-03-10 11:34:09 +0800

    Update decompassion_cpio.sh

--- decompassion_cpio.sh
+++ decompassion_cpio.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 mkdir core
 cd core
-cpio -idm <../core.cpio
+cpio -idmv <../core.cpio
+
+# 有的不讲武德，写的是 .cpio 文件，但会发现解包后是一堆乱码，其实这根本是个 .cpio.gz 文件。
+# 需要先 mv core.cpio ./core/core.cpio.gz
+# 然后 gunzip core.cpio.gz
+# 之后才可以 cpio -idmv <../core.cpio
